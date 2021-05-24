69603620e307d6505306c2442de16521f4cf15ca
rushi-t <rushikesh.talokar@gmail.com>
2021-05-24 12:15:56 +1000

    Delete market_data/upstox directory

--- market_data/upstox/start-app-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export PYTHONPATH=/home/rtalokar/win-d/work/stock_market
-# shellcheck disable=SC2164
-cd $PYTHONPATH/market_data/nse_website
-python nse_website_mdc.py NseWebsiteMdc-Dev.ini
--- market_data/upstox/start-app.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export PYTHONPATH=/home/ubuntu/projects/stock_market
-# shellcheck disable=SC2164
-cd $PYTHONPATH/market_data/nse_website
-python3.7 nse_website_mdc.py NseWebsiteMdc.ini
